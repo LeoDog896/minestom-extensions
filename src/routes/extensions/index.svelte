1c45afef24a00dbcb2ddc350a01ca4d15f78dbba
LeoDog896 <LeoDog896@hotmail.com>
2021-01-26 08:35:32 -0500

    Make extensions a little fancier

--- src/routes/extensions/index.svelte
+++ src/routes/extensions/index.svelte
@@ -13,12 +13,23 @@
 	<title>Extensions</title>
 </svelte:head>
 
+<style>
+	.extension {
+		margin-bottom: 20px;
+		border: 1px solid black;
+		padding: 10px;
+	}
+</style>
+
 <script lang="ts">
 	export let extensions: Extension[];
 </script>
 
-<section id="photos">
+<section id="extensions">
 	{#each extensions as extension}
-		<a href="extensions/{extension.slug}">{extension.name}</a><br />
+		<div class="extension">
+			<a href="extensions/{extension.slug}">{extension.name}</a>
+		<p>{extension.short_description}</p>
+		</div>
 	{/each}
 </section>
